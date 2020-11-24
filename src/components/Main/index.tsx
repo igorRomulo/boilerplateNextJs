04a701794cf462c34d1dbb51b172d3bdc66efe30
Igor <igor.romulo.santos@gmail.com>
2020-11-23 21:54:12 -0300

    Remove import no used

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,15 @@
+import { Wrapper, Title, Description, Logo, Illustration } from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <Wrapper>
+    <Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <Title>React Avançado</Title>
+    <Description>TypeScript, ReactJS, NextJS e Styled Components</Description>
+    <Illustration src="/img/hero-illustration.svg" />
+  </Wrapper>
 )
 
 export default Main
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  align-items: center;
+  background-color: #06092b;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  padding: 3rem;
+  text-align: center;
+`
+
+export const Logo = styled.img`
+  margin-bottom: 2rem;
+  width: 25rem;
+`
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+`
+
+export const Description = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
+  margin-bottom: 3rem;
+`
+
+export const Illustration = styled.img`
+  width: min(30rem, 100%);
+`
